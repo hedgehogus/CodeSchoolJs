4dd3fdb9c93594e2a7739140cd51f6edea82c712
hedgehogus <research162@gmail.com>
2017-05-30 07:03:17 +0300

    returning functions and immediate invocations

--- func.js
+++ func.js
@@ -9,3 +9,33 @@ var numbers = [15,4,3,9,8,6,10,1];
 var result = numbers.map(function (arrayCell){return arrayCell*2; });
 
 console.log("map results: " + result); 
+
+// returning functions and immediate invocations
+
+var parkRides = [["Birch Bumpers", 40],
+                 ["pines Plunge", 55],
+                 ["Cedar Coaster", 20],
+                 ["Ferris wheel of Firs", 90]];
+
+var fastPassQueue = ["Cedar Coaster", "pines Plunge", "Birch Bumpers",  "Ferris wheel of Firs"];
+
+var wantsRide = "Cedar Coaster";
+
+function buildTicket(allRides, passRides, pick) {
+    if (passRides[0] == pick){
+        var pass = passRides.shift();
+        return function () { alert("Quick! You've got a Fast Pass to " + pass + "!");};
+    } else {
+        for (var i = 0; i < allRides.length; i++){
+            if (allRides[i][0] == pick) {
+                return function (){
+                    alert("A ticket is printing for " + pick + "!\n" + " Your wait time is about " + allRides[i][1] + " minutes.");
+                }
+            }
+        }
+    }
+}
+
+var ticket = buildTicket(parkRides, fastPassQueue, wantsRide);
+
+ticket();
