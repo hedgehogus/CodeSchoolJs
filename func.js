fb7395a4d00a40f5ee37f2c9415066de051dd0dd
hedgehogus <research162@gmail.com>
2017-05-30 05:08:20 +0300

    map

--- func.js
+++ func.js
@@ -1,3 +1,11 @@
 var diff = function (a,b){
     return a*a - b*b;
 }
+
+// map
+
+var numbers = [15,4,3,9,8,6,10,1];
+
+var result = numbers.map(function (arrayCell){return arrayCell*2; });
+
+console.log("map results: " + result); 
