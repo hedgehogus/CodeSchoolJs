b93f4b469cb403b7a53a24f0187643479c389a20
hedgehogus <research162@gmail.com>
2017-06-01 06:40:02 +0300

    changing variables

--- closures.js
+++ closures.js
@@ -1,7 +1,20 @@
 
-function buildCoveTicketMaker( transport) {
+function buildCoveTicketMakerFirst( transport) {
     return function (name ) {
         alert("Here is your transportation ticket : " + transport + " .\n" +
-        "welcome to the cold closures csve, " + name + "!");
+        "welcome to the cold closures cave, " + name + "!");
     }
 }
+
+function buildCoveTicketMakerSecond( transport) {
+    var passengerNumber = 0;
+    return function (name ) {
+        passengerNumber++;
+        alert("Here is your transportation ticket : " + transport + " .\n" +
+        "welcome to the cold closures cave, " + name + "!\n" +
+        "Your are passenger #" + passengerNumber + "." );
+    }
+}
+
+var getSubmarineTicket = buildCoveTicketMakerSecond("Submarine");
+getSubmarineTicket;
