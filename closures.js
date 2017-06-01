dad800e8283847a19a3d8ccc9b5ea26c9a8f4667
hedgehogus <research162@gmail.com>
2017-06-01 20:09:00 +0300

    dangers at the moment of closure

--- closures.js
+++ closures.js
@@ -18,3 +18,38 @@ function buildCoveTicketMakerSecond( transport) {
 
 var getSubmarineTicket = buildCoveTicketMakerSecond("Submarine");
 getSubmarineTicket;
+
+// dangers at the moment of closure ( not correct number)
+function assignTorpedo( name, passengerArray){
+    var torpedoAssignment;
+    for (var i = 0; i < passengerArray.length; i++){
+        if (passengerArray[i] == name) {
+            torpedoAssignment = function(){
+                alert("Ahoy, " + name + "!\n" + "Man your post at Torpedo #" + (i+1) + "!");
+            }
+        }
+    }
+    return torpedoAssignment;
+}
+
+var subPassengers = ["Luke", "Leia", "Han", "Chewie", "Yoda", "R2D2" ];
+var giveAssignment = assignTorpedo("Chewie", subPassengers);
+//giveAssignment();
+
+// one of correct ways
+
+function makeTorpedoAssigner(passengerArray){
+    return function (name) {
+        for (var i = 0; i< passengerArray.length; i++){
+            if(passengerArray[i] == name){
+                alert("Ahoy, " + name + "!\n" + "Man your post at Torpedo #" + (i+1) + "!");
+            }
+        }
+    }
+}
+
+var getTorpedoFor = makeTorpedoAssigner(subPassengers);
+getTorpedoFor("Chewie");
+
+
+
