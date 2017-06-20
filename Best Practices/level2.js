9cf7ef49cb3a8583d02f46308bb7882f12fa9a7b
hedgehogus <research162@gmail.com>
2017-06-20 06:05:38 +0300

    inheritance and adding elements

--- Best Practices/level2.js
+++ Best Practices/level2.js
@@ -62,3 +62,62 @@ console.log(invertedPeninsula.inhabitants.humans.name);
 // <script type="text/javascript"
 //          src = "http://...js"
 //          async></script>
+
+
+// SHORT PERFORMANCE TIPS
+
+// let inheritance help with memory efficiency
+
+SignalFire.prototype = {
+    addLogs: function (numLogs){
+        this.logsLeft += numLogs;
+    },
+    lightFire: function() { 
+        alert("whoosh!");
+    },
+    smokeSignal: function (message){
+        this.message = message
+        if (this.logsLeft < this.message.length / 10){
+            alert("not enough fuel for send the current message");
+        } else {
+            this.lightFire();
+            var x = this.message.length;
+            for (var i = 0; i < x; i++){
+                alert("(((" + this.message[i] + ")))");
+                if (i % 10 == 0 && i != 0){
+                    this.logsLeft--;
+                }
+            }
+        }
+    }
+}
+
+function SignalFire(ID, startingLogs) {
+    this.fireID = ID;
+    this.logsLeft = startingLogs;
+}
+
+var fireOne = new SignalFire(1,20);
+var fireTwo = new SignalFire(2.18);
+var fireThree = new SignalFire(3,24);
+
+fireOne.addLogs(8);
+fireTwo.addLogs(10);
+fireThree.addLogs(4);
+
+// fireThree.smokeSignal("Goblins!");
+
+
+// adding individual dom elements ain't always speedy
+
+var list = document.getElementById("kotwList"),
+    kotw = ["hedgehog", "hoglet", "hedgie"],
+    fragment = document.createDocumentFragment(),
+    element;
+for (var i = 0, x = kotw.length; i < x ; i ++ ){
+    element = document.createElement( "li");
+    element.appendChild( document.createTextNode(kotw[i]));
+    fragment.appendChild(element);
+}
+list.appendChild(fragment);
+
