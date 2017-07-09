c1b5804aefa466dabd6ac9981e1c8814de4d460e
hedgehogus <research162@gmail.com>
2017-07-10 02:00:37 +0300

    using let

--- ES2015/level1.js
+++ ES2015/level1.js
@@ -1,3 +1,16 @@
 ///////////////
 // USING LET //
 ///////////////
+
+function loadProfiles(userName){
+    if (userName.length > 3){
+        let loadingMessage = "This might take a while...";
+        _displaySpinner(loadingMessage);
+    } else {
+        let flashMessage = "loading Profiles";
+        _displayFlash(flashMessage);
+    }
+}
+
+// the let keyword defines new variables scoped to the nearest block, not function
+
