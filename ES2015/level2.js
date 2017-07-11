1d78a82602f3a9ad7b50a043c5d61fb59876e794
hedgehogus <research162@gmail.com>
2017-07-12 02:02:59 +0300

    rest params

--- ES2015/level2.js
+++ ES2015/level2.js
@@ -32,3 +32,47 @@ function setPageThread(name, {popular, expires, activeClass} = {}){
     console.log(name + " " + popular + " " + expires + " " + activeClass);
     // ... 
 }
+
+/////////////////
+// REST PARAMS //
+/////////////////
+
+function displayTags(targetElement){
+    let str = "";
+    for(let i in arguments){
+        let tag = arguments[i];
+        str += tag + " ";
+    }
+    console.log(targetElement);
+    console.log(str);
+}
+
+displayTags("fff", "ddd", "aaa");
+displayTags();
+
+//the new rest parameters syntax allows us to represent an indefinite number of arguments
+// as an array. this way, changes to function signature are less likely to break code
+
+function displayTags(targetElement, ...tags){
+    let str = "";
+    for(let i in tags){
+        let tag = tags[i];
+        str += tag + " ";
+    }
+    console.log(targetElement);
+    console.log(str);
+}
+
+displayTags("target","fff", "ddd", "aaa");
+displayTags("target");
+
+///////////////
+// SPREAD OP //
+///////////////
+
+getRequest("/fgfg/fgfg/fgfg", function(data){
+    let tags = data.tags;
+    displayTags(...tags);
+});
+
+let arr = 
