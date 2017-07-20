ff7360ebbffa83bcb65089f4863ac045c2aa85a1
hedgehogus <research162@gmail.com>
2017-07-20 06:46:31 +0300

    for of loop

--- ES2015/level4.js
+++ ES2015/level4.js
@@ -17,6 +17,40 @@ console.log(a, b, c);
 console.log(a,b);
 
 // we can combine destructuring with rest parameters to group values into other arrays
-let[first, second, ...rest ];
+let[first, second, ...rest ] = users;
 console.log(first, second);
 console.log(rest);
+console.log("");
+
+// when returning arrays from functions, we can assign to multiple variables at once
+
+function activeUsers(){
+    let users = ["sam", "tyler", "alex"];
+    return users;
+}
+
+let active = activeUsers();
+console.log(active);
+
+[a,b,c] = activeUsers();
+console.log(a,b,c);
+
+[a,b,c] = active;
+console.log(a,b,c);
+
+console.log("");
+
+
+// 2. FOR ...OF loop 
+
+let names = ["sam", "tyler", "brook"];
+
+for (let index in names){
+    console.log(names[index]);
+}
+
+for (let name of names){
+    console.log(name);
+}
+
+// the for ...of statement cannot be used to iterate over properties in plain JS objects out-of-the box
