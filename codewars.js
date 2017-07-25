00a1c720ef919a76abb7bed3061f1d06af6ae458
hedgehogus <research162@gmail.com>
2017-07-25 05:46:47 +0300

    math issues 1

--- codewars.js
+++ codewars.js
@@ -354,3 +354,29 @@ function findNb(m) {
     }
     return rem === 0 ? n : -1;
 }
+
+
+
+// Oh no, our Math object was "accidently" reset. Can you re-implement some of those functions? 
+// We can assure, that only non-negative numbers are passed as arguments. So you don't have to consider 
+// things like undefined, null, NaN, negative numbers, strings and so on.
+
+// Here is a list of functions, we need:
+
+// Math.round()
+// Math.ceil()
+// Math.floor()
+
+// 1
+
+Math.round = function(number) {
+  return number%1 < 0.5 ? Math.floor(number) : Math.ceil(number)  ; 
+};
+
+Math.ceil = function(number) {
+  return number%1>0 ? number - number%1 + 1 : number; 
+};
+
+Math.floor = function(number) {
+  return number - number%1;
+  };
