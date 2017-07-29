01c88c5abacceb2837d73e257348f354468ba30c
hedgehogus <research162@gmail.com>
2017-07-29 05:09:36 +0300

    classes

--- ES2015/level5.js
+++ ES2015/level5.js
@@ -1,3 +1,49 @@
 /////////////
 // CLASSES //
 /////////////
+
+// a common approach to encapsulation in JS is using a constructor function
+
+function SponsorWidget(name, description, url){
+    this.name = name;
+    this.description = description;
+    this.url = url;
+}
+
+SponsorWidget.prototype.render = function(){
+    //...
+};
+
+// invoking the SponsorWidget function looks like this:
+
+let sponsorWidget = new SponsorWidget(name, description, url);
+sponsorWidget.render();
+
+// using the nee class syntax
+
+class SponsorWidget {
+
+    constructor(name, description, url){
+        this.name = name;
+    this.description = description;
+    this.url = url;
+    }
+
+    render(){
+        let link = this._buildLink(this.url);
+        //...
+    }
+
+    // prefixing a method with an underscore is a convention for indicating 
+    // that it should not be invoked from the public API
+
+    _buildLink(url){
+        //...
+    }
+}
+
+// the SponsorWidget function still use it just like before
+
+let sponsorWidget = new SponsorWidget(name, description, url);
+sponsorWidget.render();
+
