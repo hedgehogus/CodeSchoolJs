c874b6620e200de076419316e2dd521862ed0fd4
hedgehogus <research162@gmail.com>
2017-08-04 01:54:58 +0300

    Simple fraction to mixed number converter

--- codewars2.js
+++ codewars2.js
@@ -39,3 +39,37 @@ function findDividers(n) {
   console.timeEnd("hedge");
   return ds;
 }
+
+
+
+// Simple fraction to mixed number converter
+
+function mixedFraction(s){
+  let arr = s.split("/");
+  if (arr[1] == 0){
+    throw new IllegalArgumentException("ZeroDivisionError");
+  }
+  let int = parseInt(arr[0]/arr[1]);
+  let rem = arr[0]-int*arr[1];
+  let div = arr[1];
+  if (int !== 0 || (rem < 0 && div < 0)){
+   rem = Math.abs( arr[0]-int*arr[1]);
+   div = Math.abs( arr[1]);
+  }
+
+   for (let i = 2; i <= rem; i++){
+      let repeat = true;
+      while (repeat){
+      let a1 = rem/i;
+      let a2 = div/i;
+      if (Number.isInteger(a1) && Number.isInteger(a2)){
+        rem = a1;
+        div = a2;
+      } else {
+        repeat = false;
+      }
+      }
+  }
+ 
+  return (int === 0 ? rem === 0 ? 0 :"" : rem === 0 ? int : int + " " ) + (rem === 0 ? "" : rem + "/" + div);
+}
