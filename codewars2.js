75230aa7600d900db4e0d9d5f28a784b6d8886e7
hedgehogus <research162@gmail.com>
2017-08-04 17:32:24 +0300

    solution

--- codewars2.js
+++ codewars2.js
@@ -1,3 +1,8 @@
+
+//Simple fraction to mixed number converter
+
+// first solution
+
 function mixedFraction(s){
   let arr = s.split("/");
   if (arr[1] == 0){
@@ -30,4 +35,37 @@ function mixedFraction(s){
   }
  
   return (int === 0 ? rem === 0 ? 0 :"" : rem === 0 ? int : int + " " ) + (rem === 0 ? "" : rem + "/" + div);
+}
+
+// correct solution 
+
+function mixedFraction(s){
+  let [rem,div] = s.split("/");
+  if (div == 0){
+    throw new IllegalArgumentException("ZeroDivisionError");
+  }
+  let int = parseInt(rem/div);
+  rem =div < 0 ? -(rem-int*div):rem-int*div;  
+  
+  let a = gcd(rem,div);
+  rem = rem/a;
+  div = Math.abs(div/a);
+  
+  if(int < 0) {
+    rem = Math.abs(rem);
+  }
+ 
+  return (int === 0 ? rem === 0 ? 0 :"" : rem === 0 ? int : int + " " ) + (rem === 0 ? "" : rem + "/" + div);
+}
+
+function gcd(a,b) {
+    a = Math.abs(a);
+    b = Math.abs(b);
+    if (b > a) {var temp = a; a = b; b = temp;}
+    while (true) {
+        if (b == 0) return a;
+        a %= b;
+        if (a == 0) return b;
+        b %= a;
+    }
 }
