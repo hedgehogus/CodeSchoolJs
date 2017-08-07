f1e6d630701fe4ec7a1d24484ab44a6bf7470745
hedgehogus <research162@gmail.com>
2017-08-07 03:03:35 +0300

    promises 2

--- ES2015/level6.js
+++ ES2015/level6.js
@@ -59,3 +59,113 @@ function getPollResultsFromServer(pollName){
         reject(someValue);
     })
 }
+
+// creating a new promise autimatically sets it to the pending state. then, it can do 1 of 2
+// things: become fullfilled (when resolve) or rejected
+
+// a promise represents a future value, such as the eventual result of an asynchronous operation
+
+let fetchingResults = getPollResultsFromServer("Sass vs. LESS"); 
+// no longer need to pass a callback function as argument
+
+
+// let's wrap the XMLHttpRequest object API within a promise.
+// Calling the resolve() handler moves the Promise to a fulfilled state
+
+function getPollResultsFromServer(pollName){
+    return new Promise(function(resolve,reject){
+        let url = `/result/${pollName}`;
+        let request = new XMLHttpRequest();
+        request.open('GET', url, true);
+        request.onload = function(){
+            if(request.status>= 200 && request.status < 400){
+                resolve(JSON.parse(request.response));
+            }
+        };
+        //...
+        request.send();
+    })
+}
+
+// we can use then() method to read results from the promise once it's resolved.
+// this method takes a function that will only be invoked once the Promise is resolved
+
+let fetchingResults = getPollResultsFromServer("Sass vs. LESS");
+fetchingResults.then(function(results){
+    ui.renderSidebar(results)
+})
+
+// we are currently using a temporary variable to store our Promise object, but it's not
+// really necessary. Lets replace it with chaining function calls
+
+getPollResultsFromServer("Sass vs. LESS")
+    .then(function(results){
+        ui.renderSidebar(results);
+    });
+
+
+// we can also chain multiple calls to then() - the return value from 1 call 
+// is passed as argument to the next
+
+getPollResultsFromServer("Sass vs. LESS")
+    .then(function(results){
+        return results.filter((result) => result.city === "Orlando");
+    })
+    .then(function(resultFromOrlando){
+        ui.renderSidebar(resultFromOrlando);
+    });
+
+// we can call the reject() handler for unsuccesful status codes and also when the onerror event
+// is trigered onour request object. Both move the promise to a reject state.
+
+function getPollResultsFromServer(pollName){
+    return new Promise(function(resolve,reject){
+        let url = `/result/${pollName}`;
+        let request = new XMLHttpRequest();
+        request.open('GET', url, true);
+        request.onload = function(){
+            if(request.status>= 200 && request.status < 400){
+                resolve(JSON.parse(request.response));
+            } else {
+                reject( new Error(request.status));
+            }
+        };
+        request.onerror = function(){
+            reject(new Error("Error fetching Results"));
+        }
+        //...
+        request.send();
+    })
+}
+
+// Once an error occurs, execution moves immediately to the catch() function. None of the 
+// remaining then() functions are invoked
+
+getPollResultsFromServer("Sass vs. LESS")
+    .then(function(results){
+        return results.filter((result) => result.city === "Orlando");
+    })
+    .then(function(resultFromOrlando){
+        ui.renderSidebar(resultFromOrlando);
+    })
+    .catch(function(error){
+        console.log("error: ". error);
+    });
+
+// we can make our code more succinct by passing function arguments to then, 
+// instead of using anonymous functions
+
+function filterResults(results){
+    return results.filter((result) => result.city === "Orlando");
+}
+
+let ui = {
+    renderSidebar(filteredResults){/*...*/}
+};
+
+getPollResultsFromServer("Sass vs. LESS")
+    .then(filterResults)
+    .then(ui.renderSidebar)
+    .catch(function(error){
+        console.log("error: ". error);
+    });
